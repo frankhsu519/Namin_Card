4fb4c80bf5ca81259c3f5ca12b6cf14da39c0ce4
NEWEBOA\frank.hsu <frank_hsu@neweb.com.tw>
2022-10-26 17:20:13 +0800

    1026- 修改路徑

--- js/data.js
+++ js/data.js
@@ -2,326 +2,325 @@ var data =[
     {
         id:0,
         title:"我是測試圖片 ",
-        img:"../data_img/測試圖片.png",
+        img:`${window.location.origin}/Namin_Card/data_img/測試圖片.png`,
         video:`${window.location.origin}/video/013.mp4`
     },
     {
         id:1,
         title:"2021 01 31 kisses",
-        img:"../data_img/20210131_kisses.png",
+        img:`${window.location.origin}/Namin_Card/data_img/20210131_kisses.png`,
         video:`${window.location.origin}/video/2021 01 31 kisses.mkv`
     },
     {
         id:2,
         title:"2021 11 11 kisses",
-        img:"../data_img/20211111_kisses.png",
+        img:`${window.location.origin}/Namin_Card/data_img/20211111_kisses.png`,
         video:`${window.location.origin}/video/2021 11 11 kisses.mkv`
     },  
     {
         id:3,
         title:"20211125_kisses",
-        img:"../data_img/20211125_kisses.png",
+        img:`${window.location.origin}/Namin_Card/data_img/20211125_kisses.png`,
         video:`${window.location.origin}/video/2021 11 25 kisses.mkv`
     },  
     {
         id:4,
         title:"2021 11 27 Kisses",
-        img:"../data_img/20211127_kisses.png",
+        img:`${window.location.origin}/Namin_Card/data_img/20211127_kisses.png`,
         video:`${window.location.origin}/video/2021 11 27 Kisses.mkv`
     },  
     {
         id:5,
         title:"2021 12 25 kisses",
-        img:"../data_img/20211225_kisses.png",
+        img:`${window.location.origin}/Namin_Card/data_img/20211225_kisses.png`,
         video:`${window.location.origin}/video/2021 12 25 kisses.mkv`
     },  
     {
         id:6,
         title:"2022 01 04 Kisses",
-        img:"../data_img/20220104_kisses.png",
+        img:`${window.location.origin}/Namin_Card/data_img/20220104_kisses.png`,
         video:`${window.location.origin}/video/2022 01 04 Kisses.mkv`
     },  
     {
         id:7,
         title:"2022 01 14 Kisses ",
-        img:"../data_img/20220114_kisses.png",
+        img:`${window.location.origin}/Namin_Card/data_img/20220114_kisses.png`,
         video:`${window.location.origin}/video/2022 01 14 Kisses.mkv`
     },  
     {
         id:8,
         title:"2022 01 20 Kisses ",
-        img:"../data_img/20220120_kisses.png",
+        img:`${window.location.origin}/Namin_Card/data_img/20220120_kisses.png`,
         video:`${window.location.origin}/video/2022 01 20 Kisses.mkv`
     },  
     {
         id:9,
         title:"2022 01 25 Kisses",
-        img:"../data_img/20220125_kisses.png",
+        img:`${window.location.origin}/Namin_Card/data_img/20220125_kisses.png`,
         video:`${window.location.origin}/video/2022 01 25 Kisses.mkv`
     },  
     {
         id:10,
         title:"《Namin Aegyo》Kisses 純淨版，電到歪頭   妳要我以後怎麼面對麥當勞叔叔娜珉nim",
-        img:"../data_img/20220126_kisses.png",
+        img:`${window.location.origin}/Namin_Card/data_img/20220126_kisses.png`,
         video:`${window.location.origin}/video/《Namin Aegyo》Kisses 純淨版，電到歪頭   妳要我以後怎麼面對麥當勞叔叔娜珉nim.mkv`
     },    
     {
         id:11,
         title:"2022 01 31 kisses ",
-        img:"../data_img/20220131_kisses.png",
+        img:`${window.location.origin}/Namin_Card/data_img/20220131_kisses.png`,
         video:`${window.location.origin}/video/2022 01 31 kisses.mkv`
     }, 
     {
         id:12,
         title:"2022 02 01 kisses",
-        img:"../data_img/20220201_kisses.png",
+        img:`${window.location.origin}/Namin_Card/data_img/20220201_kisses.png`,
         video:`${window.location.origin}/video/2022 02 01 kisses.mkv`
     },
     {
         id:13,
         title:"2022 02 09 kisses",
-        img:"../data_img/20220209_kisses.png",
+        img:`${window.location.origin}/Namin_Card/data_img/20220209_kisses.png`,
         video:`${window.location.origin}/video/2022 02 09 kisses.mkv`
     }, 
     {
         id:14,
         title:"2022 02 14 kisses",
-        img:"../data_img/20220214_kisses.png",
+        img:`${window.location.origin}/Namin_Card/data_img/20220214_kisses.png`,
         video:`${window.location.origin}/video/2022 02 14 kisses.mkv`
     }, 
     {
         id:15,
         title:"namin_20220223_Kisses ",
-        img:"../data_img/20220223_kisses.png",
+        img:`${window.location.origin}/Namin_Card/data_img/20220223_kisses.png`,
         video:`${window.location.origin}/video/namin_20220223_Kisses.mkv`
     }, 
     {
         id:16,
         title:"2022 02 28 kisses ",
-        img:"../data_img/20220228_kisses.png",
+        img:`${window.location.origin}/Namin_Card/data_img/20220228_kisses.png`,
         video:`${window.location.origin}/video/2022 02 28 kisses.mkv`
     },
     {
         id:17,
         title:"2022 03 03 kisses ",
-        img:"../data_img/20220303_kisses.png",
+        img:`${window.location.origin}/Namin_Card/data_img/20220303_kisses.png`,
         video:`${window.location.origin}/video/2022 03 03 kisses.mkv`
     },
     {
         id:18,
         title:"2022 03 12 Kisses ",
-        img:"../data_img/20220312_kisses.png",
+        img:`${window.location.origin}/Namin_Card/data_img/20220312_kisses.png`,
         video:`${window.location.origin}/video/2022 03 12 Kisses.mkv`
     },
-    //缺少0313資料,id 重寫地獄
     {
         id:19,
         title:"2022 03 13 kisses ",
-        img:"../data_img/20220317_kisses.png",
+        img:`${window.location.origin}/Namin_Card/data_img/20220317_kisses.png`,
         video:`${window.location.origin}/video/2022 03 13 kisses.mkv`
     },
     {
         id:20,
         title:"2022 03 17 kisses ",
-        img:"../data_img/20220317_kisses.png",
+        img:`${window.location.origin}/Namin_Card/data_img/20220317_kisses.png`,
         video:`${window.location.origin}/video/2022 03 17 kisses.mkv`
     },
     {
         id:21,
         title:"2022 03 30 Kisses ",
-        img:"../data_img/20220330_kisses.png",
+        img:`${window.location.origin}/Namin_Card/data_img/20220330_kisses.png`,
         video:`${window.location.origin}/video/2022 03 30 Kisses.mkv`
     },
     {
         id:22,
         title:"2022 04 09 kisses ",
-        img:"../data_img/20220409_kisses.png",
+        img:`${window.location.origin}/Namin_Card/data_img/20220409_kisses.png`,
         video:`${window.location.origin}/video/2022 04 09 kisses.mkv`
     },
     {
         id:23,
         title:"2022 04 14 Kisses ",
-        img:"../data_img/20220414_kisses.png",
+        img:`${window.location.origin}/Namin_Card/data_img/20220414_kisses.png`,
         video:`${window.location.origin}/video/2022 04 14 Kisses.mkv`
     },
     {
         id:24,
         title:"2022 04 17 Kisses ",
-        img:"../data_img/20220417_kisses.png",
+        img:`${window.location.origin}/Namin_Card/data_img/20220417_kisses.png`,
         video:`${window.location.origin}/video/2022 04 17 Kisses.mkv`
     },
     {
         id:25,
         title:"2022 04 23 Kisses ",
-        img:"../data_img/20220423_kisses.png",
+        img:`${window.location.origin}/Namin_Card/data_img/20220423_kisses.png`,
         video:`${window.location.origin}/video/2022 04 23 Kisses.mkv`
     },
     {
         id:26,
         title:"2022 04 25 Kisses ",
-        img:"../data_img/20220425_kisses.png",
+        img:`${window.location.origin}/Namin_Card/data_img/20220425_kisses.png`,
         video:`${window.location.origin}/video/2022 04 25 Kisses.mkv`
     },
     {
         id:27,
         title:"2022 04 28 Kisses ",
-        img:"../data_img/20220428_kisses.png",
+        img:`${window.location.origin}/Namin_Card/data_img/20220428_kisses.png`,
         video:`${window.location.origin}/video/2022 04 28 Kisses.mkv`
     },
     {
         id:28,
         title:"2022 04 30 kisses ",
-        img:"../data_img/20220430_kisses.png",
+        img:`${window.location.origin}/Namin_Card/data_img/20220430_kisses.png`,
         video:`${window.location.origin}/video/2022 04 30 kisses.mkv`
     },
     {
         id:29,
         title:"2022 05 05 kisses ",
-        img:"../data_img/20220505_kisses.png",
+        img:`${window.location.origin}/Namin_Card/data_img/20220505_kisses.png`,
         video:`${window.location.origin}/video/2022 05 05 kisses.mkv`
     },
     {
         id:30,
         title:"2022 05 07 Kisses ",
-        img:"../data_img/20220507_kisses.png",
+        img:`${window.location.origin}/Namin_Card/data_img/20220507_kisses.png`,
         video:`${window.location.origin}/video/2022 05 07 Kisses.mkv`
     },
     {
         id:31,
         title:"2022 05 13 kisses ",
-        img:"../data_img/20220513_kisses.png",
+        img:`${window.location.origin}/Namin_Card/data_img/20220513_kisses.png`,
         video:`${window.location.origin}/video/2022 05 13 kisses.mkv`
     },
     {
         id:32,
         title:"2022 05 17 kisses ",
-        img:"../data_img/20220517_kisses.png",
+        img:`${window.location.origin}/Namin_Card/data_img/20220517_kisses.png`,
         video:`${window.location.origin}/video/2022 05 17 kisses.mkv`
     },
     {
         id:33,
         title:"2022 05 18 kisses ",
-        img:"../data_img/20220518_kisses.png",
+        img:`${window.location.origin}/Namin_Card/data_img/20220518_kisses.png`,
         video:`${window.location.origin}/video/2022 05 18 kisses.mkv`
     },
     {
         id:34,
         title:"2022 05 19 kisses ",
-        img:"../data_img/20220519_kisses.png",
+        img:`${window.location.origin}/Namin_Card/data_img/20220519_kisses.png`,
         video:`${window.location.origin}/video/2022 05 19 kisses.mkv`
     },
     {
         id:35,
         title:"2022 05 22 kissess ",
-        img:"../data_img/20220522_kisses.png",
+        img:`${window.location.origin}/Namin_Card/data_img/20220522_kisses.png`,
         video:`${window.location.origin}/video/NAMIN-KISSES(1006版)_20220522.mkv`
     },
     {
         id:36,
         title:"2022 05 31 Kisses ",
-        img:"../data_img/20220531_kisses.png",
+        img:`${window.location.origin}/Namin_Card/data_img/20220531_kisses.png`,
         video:`${window.location.origin}/video/2022 05 31 Kisses.mkv`
     },
     {
         id:37,
         title:"2022 06 12 kisses ",
-        img:"../data_img/20220612_kisses.png",
+        img:`${window.location.origin}/Namin_Card/data_img/20220612_kisses.png`,
         video:`${window.location.origin}/video/2022 06 12 kisses.mkv`
     },
     {
         id:38,
         title:"2022 06 17 Kisses ",
-        img:"../data_img/20220617_kisses.png",
+        img:`${window.location.origin}/Namin_Card/data_img/20220617_kisses.png`,
         video:`${window.location.origin}/video/2022 06 17 Kisses.mkv`
     },
     {
         id:39,
         title:"2022 07 07 Kisses ",
-        img:"../data_img/20220707_kisses.png",
+        img:`${window.location.origin}/Namin_Card/data_img/20220707_kisses.png`,
         video:`${window.location.origin}/video/2022 07 07 Kisses.mkv`
     },
     {
         id:40,
         title:"2022 07 30 Kisses ",
-        img:"../data_img/20220730_kisses.png",
+        img:`${window.location.origin}/Namin_Card/data_img/20220730_kisses.png`,
         video:`${window.location.origin}/video/2022 07 30 Kisses.mkv`
     },
     {
         id:41,
         title:"Namin  -  Kisses 20220804 ",
-        img:"../data_img/20220804_kisses.png",
+        img:`${window.location.origin}/Namin_Card/data_img/20220804_kisses.png`,
         video:`${window.location.origin}/video/Namin  -  Kisses 20220804.mkv`
     },
     {
         id:42,
         title:"Namin  -  Kisses 20220810 ",
-        img:"../data_img/20220810_kisses.png",
+        img:`${window.location.origin}/Namin_Card/data_img/20220810_kisses.png`,
         video:`${window.location.origin}/video/Namin  -  Kisses 20220810.mkv`
     },
     {
         id:43,
         title:"Namin  -  Kisses 20220812 💙 ",
-        img:"../data_img/20220812_kisses.png",
+        img:`${window.location.origin}/Namin_Card/data_img/20220812_kisses.png`,
         video:`${window.location.origin}/video/Namin  -  Kisses 20220812 💙.mkv`
     },
     {
         id:44,
         title:"Namin  -  Kisses 20220822 ",
-        img:"../data_img/20220822_kisses.png",
+        img:`${window.location.origin}/Namin_Card/data_img/20220822_kisses.png`,
         video:`${window.location.origin}/video/Namin  -  Kisses 20220822.mkv`
     },
     {
         id:45,
         title:"Namin  -  Kisses 20220830 ",
-        img:"../data_img/20220830_kisses.png",
+        img:`${window.location.origin}/Namin_Card/data_img/20220830_kisses.png`,
         video:`${window.location.origin}/video/Namin  -  Kisses 20220830.mkv`
     },
     {
         id:46,
         title:"Namin  -  Kisses 20220901 ",
-        img:"../data_img/20220901_kisses.png",
+        img:`${window.location.origin}/Namin_Card/data_img/20220901_kisses.png`,
         video:`${window.location.origin}/video/Namin  -  Kisses 20220901.mkv`
     },
     {
         id:47,
         title:"2022 09 07 Kisses ",
-        img:"../data_img/20220907_kisses.png",
+        img:`${window.location.origin}/Namin_Card/data_img/20220907_kisses.png`,
         video:`${window.location.origin}/video/2022 09 07 Kisses.mkv`
     },
     {
         id:48,
         title:"Namin  -  kisses 20220908 ",
-        img:"../data_img/20220908_kisses.png",
+        img:`${window.location.origin}/Namin_Card/data_img/20220908_kisses.png`,
         video:`${window.location.origin}/video/Namin  -  kisses 20220908.mkv`
     },
     {
         id:49,
         title:"Namin  -  Kisses 20220910 💙 ",
-        img:"../data_img/20220910_kisses.png",
+        img:`${window.location.origin}/Namin_Card/data_img/20220910_kisses.png`,
         video:`${window.location.origin}/video/Namin  -  Kisses 20220910 💙.mkv`
     },
     {
         id:50,
         title:"Namin  -  kisses 20220929 💙 ",
-        img:"../data_img/20220929_kisses.png",
+        img:`${window.location.origin}/Namin_Card/data_img/20220929_kisses.png`,
         video:`${window.location.origin}/video/Namin  -  kisses 20220929 💙.mkv`,
     },
     {
         id:51,
         title:"Namin  -  Kisses 20221004",
-        img:"../data_img/20221004_kisses.png",
+        img:`${window.location.origin}/Namin_Card/data_img/20221004_kisses.png`,
         video:`${window.location.origin}/video/Namin  -  Kisses 20221004.mkv`,
     },
     {
         id:52,
         title:"Namin  -  Kisses 20221006  💗💗",
-        img:"../data_img/20221006_kisses.png",
+        img:`${window.location.origin}/Namin_Card/data_img/20221006_kisses.png`,
         video:`${window.location.origin}/video/Namin  -  Kisses 20221006  💗💗.mkv`,
     },
     {
         id:53,
         title:"Namin  - kisses 20221014 💙 ",
-        img:"../data_img/20221014_kisses.png",
+        img:`${window.location.origin}/Namin_Card/data_img/20221014_kisses.png`,
         video:`${window.location.origin}/video/Namin  - kisses 20221014.mkv`,
     },
 ]
@@ -331,43 +330,43 @@ var fail_data = [
     {      
         id:0,
         title:"我喜歡你 ",
-        img:"../data_img/fail_gif/punch_long.gif",
+        img:`${window.location.origin}/Namin_Card/data_img/fail_gif/punch_long.gif`,
         audio:`${window.location.origin}/fail_audio//Are_U_serious.mp33` 
     },
     {      
         id:1,
         title:"punch ",
-        img:"../data_img/fail_gif/punch.gif",
+        img:`${window.location.origin}/Namin_Card/data_img/fail_gif/punch.gif`,
         audio:`${window.location.origin}/fail_audio/Are_U_serious.mp3` 
     },
     {      
         id:2,
         title:"Ur_fail ",
-        img:"../data_img/fail_gif/hit.gif",
+        img:`${window.location.origin}/Namin_Card/data_img/fail_gif/hit.gif`,
         audio:`${window.location.origin}/fail_audio/Ur_fail.mp3` 
     },
     {      
         id:3,
         title:"w_comfort ",
-        img:"../data_img/fail_gif/w_comfort.gif",
+        img:`${window.location.origin}/Namin_Card/data_img/fail_gif/w_comfort.gif`,
         audio:`${window.location.origin}/fail_audio/Are_U_serious.mp3` 
     },
     {      
         id:4,
         title:"w_heavy_punch ",
-        img:"../data_img/fail_gif/w_heavy_punch.gif",
+        img:`${window.location.origin}/Namin_Card/data_img/fail_gif/w_heavy_punch.gif`,
         audio:`${window.location.origin}/fail_audio/Are_U_serious.mp3` 
     },
     {      
         id:5,
         title:"w_honey_punch ",
-        img:"../data_img/fail_gif/w_honey_punch.gif",
+        img:`${window.location.origin}/Namin_Card/data_img/fail_gif/w_honey_punch.gif`,
         audio:`${window.location.origin}/fail_audio/Are_U_serious.mp3` 
     },
     {      
         id:7,
         title:"r_honey_punch ",
-        img:"../data_img/r_honey_punch.gif",
+        img:`${window.location.origin}/Namin_Card/data_img/r_honey_punch.gif`,
         audio:`${window.location.origin}/video/Produce.m4a` 
     },
 ]
